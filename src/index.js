00c3a496fed7962973390aea985c06a10d22a71b
5924176554@qq.com <592416554@qq.com>
2024-10-21 15:59:38 +0800

    使用http-proxy-middleware设置代理

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+// 多ES6内置api做兼容处理
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/ie9";
+import "react-app-polyfill/stable";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "@/index.less"
+
+import "@/index.less";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<div>demo</div>);
+root.render(<div>demo1</div>);
